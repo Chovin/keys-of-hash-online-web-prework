ee12744912c4f93e81838c8cba4aebf8d788cf52
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-12 06:45:56 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,8 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*values)
+    keys = []
+    values.each do |value|
+      keys << self.key(value) if self.value? value
+    end
   end
 end
