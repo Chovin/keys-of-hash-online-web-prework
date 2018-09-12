39db35087647dcd2e7ed3abb210d25d297f7e7c0
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-12 06:49:59 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*values)
+    keys = []
+    values.each do |value|
+      keys.concat self.select {|k, v| v == value}.keys
+    end
+    keys
   end
 end
